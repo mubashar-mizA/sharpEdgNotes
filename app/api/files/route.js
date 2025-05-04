fb6174c2710c5b7b308ab5d2ea35632766c3ae72
mubashar-mizA <mirzamubashar535@gmail.com>
2025-05-04 17:41:01 +0500

    updating app/api/files/route.js

--- app/api/files/route.js
+++ app/api/files/route.js
@@ -1,7 +1,45 @@
 // app/api/files/route.js
 import { NextResponse } from "next/server";
-import ConnectDB from "@/configs/connectDB";
-import File from "@/models/File";
+import ConnectDB from "@/app/configs/connectDB";
+import File from "@/app/models/File";
+
+export async function POST(req) {
+  ConnectDB();
+  const body = await req.json();
+
+  const {
+    fileUrl,
+    fileName,
+    fileSize,
+    fileType,
+    tags,
+    description,
+    thumbnailUrl,
+    isPublic,
+  } = body;
+
+  try {
+    const newFile = await File.create({
+      fileUrl,
+      fileName,
+      fileSize,
+      fileType,
+      tags,
+      description,
+      thumbnailUrl,
+      isPublic,
+      status: "pending",
+    });
+
+    return NextResponse.json({ success: true, file: newFile });
+  } catch (err) {
+    return NextResponse.json(
+      { success: false, error: err.message },
+
+      { status: 500 }
+    );
+  }
+}
 
 export async function GET() {
   try {
